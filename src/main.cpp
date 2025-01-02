766a5cce867208ee2e07ba134453281dc4dff245
aabolfazl <aabolfazlit@gmail.com>
2025-01-02 19:48:18 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2025 Market Data Handler project
+ *
+ * This is the source code of the Market Data Handler project.
+ * It is licensed under the MIT License; you should have received a copy
+ * of the license in this archive (see LICENSE).
+ *
+ * Author: Abolfazl Abbasi
+ *
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Market Data Handler project going to the moon 🚀!" << std::endl;
+    
+    return 0;
+}

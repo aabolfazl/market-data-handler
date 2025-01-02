78ae02dbbbe80fcb54da1f9fc77db9e4210a5f01
aabolfazl <aabolfazlit@gmail.com>
2025-01-02 21:12:53 +0300

    [feat] add logger

---
+++ src/define.hpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2025 Market Data Handler project
+ *
+ * This is the source code of the Market Data Handler project.
+ * It is licensed under the MIT License; you should have received a copy
+ * of the license in this archive (see LICENSE).
+ *
+ * Author: Abolfazl Abbasi
+ *
+ */
+
+#pragma once
+
+// #define ULTRA_LOW_LATENCY_MODE
--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,17 @@
  */
 
 #include <iostream>
+#include "logger/logger.cpp"
 
 int main() {
     std::cout << "Market Data Handler project going to the moon 🚀!" << std::endl;
-    
+    logger::init();
+
+    TRACE_LOG("This is a trace log message");
+    DEBUG_LOG("This is a debug log message");
+    INFO_LOG("This is an info log message");
+    WARN_LOG("This is a warning log message");
+    ERROR_LOG("This is an error log message");
+
     return 0;
 }
